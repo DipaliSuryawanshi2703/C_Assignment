9003e40f99ae2a8e3911a3a83150b8e2cc0ace8a
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-25 11:04:13 +0530

    Add files via upload

---
+++ 01Assignment/03 Addition.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Number1 = 0 ,Number2 = 0 ,Sum = 0;
+
+    printf("\n Enter the Number1 = ");
+    scanf("%d",&Number1);
+
+    printf("\n\n Enter the Number2 = ");
+    scanf("%d",&Number2);
+
+    Sum= Number1 + Number2;
+    printf("\n\n Addition Of Both Numbers = %d + %d = %d.",Number1,Number2,Sum);
+
+    getch();
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
