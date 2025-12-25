352081f480a630ca07f2693da07e46902709057f
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-25 22:39:03 +0530

    Update 15  M To  KM.c

--- 01Assignment/15  M To  KM.c
+++ 01Assignment/15  M To  KM.c
@@ -6,15 +6,16 @@ int main()
     float Kilometer = 0;
     float Meter = 0;
 
-    printf("\n Convert The Kilometer To Meter =   ");
+    printf("\n Convert The Meter to  Kilometer =   ");
 
 
     printf("\n Enter Distance In Meter");
     scanf("%f",&Meter);
 
     Kilometer = Meter/1000;
-    printf("\n Given Distance is %0.2f =%0.3f ",Meter, Kilometer);
+    printf("\n Given Distance is %0.2fm =%0.3fkm ",Meter, Kilometer);
 
     getch ();
     return 0;
 }
+
