4ce46061e349e570d2dd42b6e74a5377c5a16b23
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-25 11:00:19 +0530

    Add files via upload

---
+++ 01Assignment/01 Display Message.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n\a Welcome To C Programming");
+
+    printf("\n\n It Was MY First Code");
+
+    printf("\n\n Name\t Age\t Gender");
+/*\a used for bip sound*/
+/*\t is used for spacing in two word
+&\n used for spacing in two line*/
+
+    getch();
+    return  0;
+}
