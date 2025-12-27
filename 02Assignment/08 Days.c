602d367a871d7b7b74b4faa66d6bca225de2235d
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-27 20:44:42 +0530

    08 Days.c

--- 02Assignment/08 Days.c
+++ 02Assignment/08 Days.c
@@ -3,46 +3,44 @@
 
 int main()
 {
-
-
- int Number=0;
+ int Days = 0;
 
  printf("\n Write Here Any Number From 1-7 ");
- scanf("%d",&Number);
+ scanf("%d",&Days);
 
- if(Number==1)
+ if(Days == 1)
  {
      printf("\n Monday- Important Notes");
      printf("\n To visit Mahadev mandir");
  }
- else if(Number==2)
+ else if(Days == 2)
  {
      printf("\n Tuesday- Important Notes");
      printf("\n It is student meeting day");
  }
- else if(Number==3)
+ else if(Days == 3)
  {
     printf("\n Wednesday- Important Notes");
     printf("Practice C-Programming Assignment");
  }
 
- else if(Number==4)
+ else if(Days == 4)
  {
      printf("\n Thursday-Important Note");
      printf("\n Sumit Your Yesterday Assignment");
 
  }
- else if(Number==5)
+ else if(Days == 5)
  {
     printf("\n Friday- Important Notes");
     printf("\n #c Lecture ");
  }
- else if(Number==6)
+ else if(Days == 6)
  {
      printf("\n Saturday -Important Notes");
      printf("\n Join online workshop");
  }
- else if(Number==7)
+ else if(Days == 7)
  {
      printf("\n Sunday-Important Notes");
      printf("\n Enjoy Your Holiday");
@@ -57,6 +55,7 @@ int main()
     getch();
     return 0;
  }
+
 
 
 
