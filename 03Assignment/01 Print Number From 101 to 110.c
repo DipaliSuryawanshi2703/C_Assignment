853b605bdf62e3201a0c7457acf9c6add5a5c3ca
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-27 21:23:56 +0530

    01 Print Number From 101 to 110.c

--- 03Assignment/01 Print Number From 101 to 110.c
+++ 03Assignment/01 Print Number From 101 to 110.c
@@ -2,17 +2,18 @@
 #include<conio.h>
 
 int main()
- {
-     int Num = 101;
+{
+   int Num = 101;
+   printf("\n Numbers From 101 to 110.");
 
-     printf("\n Numbers From 101 to 110.");
-
-        while (Num <= 110)
-    {       printf("\n %d",Num);
-            Num++;
-    }
+  while (Num <= 110)
+   {      
+       printf("\n %d",Num);
+       Num++;
+   }
     getch();
     return 0;
- }
+}
+
 
 
