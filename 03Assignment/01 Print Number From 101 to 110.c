377fcf4b75bcc0577766b81f827a6580b307909a
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-27 21:21:21 +0530

    Add files via upload

---
+++ 03Assignment/01 Print Number From 101 to 110.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+ {
+     int Num = 101;
+
+     printf("\n Numbers From 101 to 110.");
+
+        while (Num <= 110)
+    {       printf("\n %d",Num);
+            Num++;
+    }
+    getch();
+    return 0;
+ }
+
+
---
+++ 03Assignment/01B Print Numbers Using For.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 1;
+
+    printf("\n Numbers From 1 to 50 ");
+
+    for(int Num = 1; Num <= 50; )
+    {
+       printf("\n %d",Num);
+        Num++;
+    }
+
+    printf("\n Thank You!!!!");
+
+    getch();
+    return 0;
+}
