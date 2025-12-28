774bc2c639f3ec6e092dffe8183881aa41795d55
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2025-12-28 11:08:30 +0530

    01B Print Numbers Using For.c

--- 03Assignment/01B Print Numbers Using For.c
+++ 03Assignment/01B Print Numbers Using For.c
@@ -3,18 +3,16 @@
 
 int main()
 {
-    int Num = 1;
+    int Num = 0;
 
     printf("\n Numbers From 1 to 50 ");
 
-    for(int Num = 1; Num <= 50; )
+    for(int Num = 1; Num <= 50; Num++)
     {
        printf("\n %d",Num);
-        Num++;
     }
-
     printf("\n Thank You!!!!");
-
     getch();
     return 0;
 }
+
