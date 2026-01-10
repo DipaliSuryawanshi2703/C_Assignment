716fb314ab66f8160114c3ae7907f7bd16fbe857
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-10 16:12:58 +0530

    Add files via upload

---
+++ 07Assignment/03 -7 Elements with Null (0) Initialization & Display All Elements.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+
+    int No[7]= {12,23,44,-67,890,672,78};
+
+    printf("\n Enter Five Numbers ==> \n");
+
+    printf("\n First Number is %d ",No[0]);
+
+    printf("\n Second Number is %d ",No[1]);
+
+    printf("\n Third Number is %d ",No[2]);
+
+    printf("\n Fourth Number is %d ",No[3]);
+
+    printf("\n Fifth Number is %d ",No[4]);
+    ;
+    printf("\n Sixth Number is %d ",No[5]);
+
+    printf("\n Sevanth Number is %d ",No[6]);
+
+
+    getch();
+    return 0;
+}
+
+
