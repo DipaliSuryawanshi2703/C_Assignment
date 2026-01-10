243e9d6af863db7c982d13782095327a267e888e
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-10 16:12:12 +0530

    Add files via upload

---
+++ 07Assignment/01 Array Without Initialization & Display All Elements.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+
+    int No[5];
+
+    printf("\n Enter Five Numbers ==> \n");
+
+    printf("\n First Number = %d",No[0]);
+    printf("\n Second Number = %d",No[1]);
+    printf("\n Third Number = %d",No[2]);
+    printf("\n Fourth Number = %d",No[3]);
+    printf("\n Fifth Number = %d",No[4]);
+
+    getch();
+    return 0;
+}
