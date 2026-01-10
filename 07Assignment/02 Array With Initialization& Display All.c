412bbc903e559926b3be6c20770528c3db64f107
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-10 16:12:38 +0530

    Add files via upload

---
+++ 07Assignment/02 Array With Initialization& Display All.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+
+    int No[7]= {12,23,45,67,-34,45,90};
+
+    printf("\n Enter Five Numbers ==> \n");
+
+    printf("\n First Number = %d",No[0]);
+    printf("\n Second Number = %d",No[1]);
+    printf("\n Third Number = %d",No[2]);
+    printf("\n Fourth Number = %d",No[3]);
+    printf("\n Fifth Number = %d",No[4]);
+    printf("\n Fixth Number = %d",No[5]);
+    printf("\n Sevanth Number = %d",No[6]);
+
+
+    getch();
+    return 0;
+}
+
