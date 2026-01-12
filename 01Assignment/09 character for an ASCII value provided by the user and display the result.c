bcf29c8a1bb578adff0bae70a11bf0bbbb227510
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-12 14:25:24 +0530

    Add files via upload

---
+++ 01Assignment/09 character for an ASCII value provided by the user and display the result.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+
+     printf("\n Enter ASCII Value For Character = ");
+     scanf("%d",&No);
+
+     printf("\n The Character Of Given %d Is '%c'",No,No);
+
+     getch ();
+     return 0;
+
+
+}
