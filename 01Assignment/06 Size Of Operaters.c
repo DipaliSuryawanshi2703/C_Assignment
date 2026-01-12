b64833380773bf020164bc61d125c8be1f4586b2
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-12 14:24:25 +0530

    Add files via upload

---
+++ 01Assignment/06 Size Of Operaters.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    double no=0;
+    int Num=0;
+
+    printf("\n Size Of Variables ==>  ");
+
+    printf("\n %d Byte", sizeof(char));
+    printf("\n %d Bytes", sizeof(int));
+    printf("\n %d Bytes", sizeof(float));
+    printf("\n %d Bytes", sizeof(double));
+    printf("\n %d Bytes", sizeof(Num));
+
+    getch();
+    return 0;
+}
