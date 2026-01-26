255d41a7a08ba75bb39711edfb281c7bdd28a623
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-01-26 10:45:34 +0530

    Add files via upload

---
+++ 07Assignment/17 Find Maximum & 2nd Maximum Element in that Array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 10
+
+int main()
+{
+    int Num[Size] = {}, i = 0, Max = 0, flag =-1, Max2;
+
+    printf("\n Enter The All Element =>  \n");
+
+    for(i = 0; i < Size ; i++)
+    {
+        printf("\n Enter The Number %d : ",i+1);
+        scanf("%d",&Num[i]);
+        if(i == 0 || Num[i] > Max)
+        {
+            Max = Num[i];
+        }
+      if (i == 0 || Num[i] != Max)
+
+        if(flag = -1 || Num[i] > Max2)
+        {
+            Max2 = Num[i];
+        }
+        flag = 1;
+    }
+
+    printf("\n ===================************=================");
+
+    printf("\n The Maximum Number From Given Element Is %d \n",Max);
+    printf("\n The Second Maximum Number From Given Element Is %d ",Max2);
+    getch();
+    return 0;
+}
+
