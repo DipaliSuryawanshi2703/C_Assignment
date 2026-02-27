3838ada282d6dc019d28747a887362ec9905ee05
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-02-27 10:00:18 +0530

    Add files via upload

---
+++ 04 Assignment Pattern/07 Print Number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int i = 0, j = 0, R = 0, C = 0, Num = 1;
+
+    printf("\n Enter The Row Count =>   ");
+    scanf("%d",&R);
+    printf("\n Enter The Column Count => ");
+    scanf("%d",&C);
+    printf("\n");
+
+    for(i = 1 ; i <= R ; i++)
+    {
+        for( j = 1; j <= C; j++)
+        {
+            printf(" %3d ",Num);
+            Num++;
+
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+
+
+
