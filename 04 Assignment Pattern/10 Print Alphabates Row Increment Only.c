043e0589074d3922fa56988cb7369b5ae7c128c6
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-03-02 10:38:58 +0530

    Add files via upload

---
+++ 04 Assignment Pattern/10 Print Alphabates Row Increment Only.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int i = 0, j = 0, R = 0, C = 0 ;
+    char Ch = 'A';
+
+    printf("\n Enter The Row Count =>   ");
+    scanf("%d",&R);
+    printf("\n Enter The Column Count => ");
+    scanf("%d",&C);
+    printf("\n");
+
+    for( i = 1 ; i <= R ; i++, Ch++)
+    {
+        for( j = 1; j <= C; j++)
+        {
+            printf(" %c ",Ch);
+
+
+            if(Ch > 'Z')
+            {
+                Ch = 'A';
+            }
+        }
+
+
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+
+
+
+
+
