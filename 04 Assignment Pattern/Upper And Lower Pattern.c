01093c48879e00e9a6a0bf68915cfa49a3b2c651
Dipali Suryawanshi <suryawanshidipali482@gmail.com>
2026-04-21 17:51:15 +0530

    Add files via upload

---
+++ 04 Assignment Pattern/Upper And Lower Pattern.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int n , j ,i;
+
+    printf("\n Enter the Row Of The Pyramid =   ");
+    scanf("%d",&n);
+
+    for(i = 1; i < (n+1)/2; i++ )
+    {
+        for(j = 1; j <= (n+1)/2 - i; j++)
+        {
+
+            printf("   ");
+        }
+        for(j = 1; j <= (2*i)-1; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+     for(i = 1; i <= n/2; i++ )
+    {
+        for(j = 1; j <=  i; j++)
+        {
+            printf("   ");
+        }
+        for(j = 1; j <= n - (2*i); j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
+
